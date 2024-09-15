c541827104a470773f7df03cc4c936cccf23c4f3
cuitao223 <1972687818@qq.com>
2024-09-15 21:45:52 +0800

    Add files via upload

---
+++ nm_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int add(int x, int y)
+{
+	return x + y;
+}
+ 
+int global_a;
+int global_b = 1;
+char szTest[] = "Hello";
+ 
+int main()
+{
+	int part_c = 2;
+	return 0;
+}
+
